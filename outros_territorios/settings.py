f58cf663ad36f109c12a044cf42fbc1b63169ac1
acoutinhoa <acoutinhoa@gmail.com>
2018-10-19 14:00:49 -0300

    settings.py

--- outros_territorios/settings.py
+++ outros_territorios/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-# deleeeta
+# e agora?
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
