dd8c9a3cee657853dc8c262f013e7340634e1e40
acoutinhoa <acoutinhoa@gmail.com>
2018-10-21 19:20:02 -0300

    wsgi

--- outros_territorios/wsgi.py
+++ outros_territorios/wsgi.py
@@ -7,10 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-import os
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "outros_territorios.settings")
+
+# application = get_wsgi_application()
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "outros_territorios.settings")
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
